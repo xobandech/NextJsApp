c6edd518cf4d44394e276b6c3c06ba8386451532
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-05 15:50:50 +0200

    Make correct redirect on back to shoppage

--- src/app/shop/cart/page.tsx
+++ src/app/shop/cart/page.tsx
@@ -1,7 +1,14 @@
-export default function CartPage () {
-    return (
-        <h1>
-            CartPage
-        </h1>
-    )
+"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+const handleBackClick = () => {
+  redirect("/shop");
+};
+export default function CartPage() {
+  return (
+    <>
+      <h1>ShopPage</h1>
+      <Link href="../shop" onClick={handleBackClick}>Back to shop</Link>
+    </>
+  );
 }
