76fd45935be1526e9662a52a3292d198b3476907
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-07 19:48:48 +0200

    Delete functions file

--- src/app/shop/functions.tsx
+++
@@ -1,8 +0,0 @@
-"use server"
-const { PrismaClient } = require("@prisma/client");
-
-export const getProducts = async (): Promise<Product[]> => {
-    "use server";
-    const prisma = new PrismaClient();
-    return await prisma.product.findMany();
-  };
