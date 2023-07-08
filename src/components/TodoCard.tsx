b77d4b5348ebc0d90b23e7ebb5930af8b8c4736a
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-08 14:36:52 +0200

    Remake toggleTodo to save info in db

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,10 +1,21 @@
-"use client"
+"use client";
 import { TodoCardProps } from "@/types/TodoCardProps";
 export function TodoCard({ id, title, completed, toggleTodo }: TodoCardProps) {
   return (
     <li className="flex gap-1 items-center">
-        <input type="checkbox" id={id} className="cursor-pointer peer" onChange={(e) => toggleTodo(e.target.checked, id)} />
-        <label htmlFor={id} className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500">{title}</label>
+      <input
+        type="checkbox"
+        id={id}
+        className="cursor-pointer peer"
+        defaultChecked={completed}
+        onChange={(e) => toggleTodo(e.target.checked, id)}
+      />
+      <label
+        htmlFor={id}
+        className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
     </li>
   );
 }
