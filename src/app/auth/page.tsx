458129129bd5bfe5c49b0d57c358784efdfb525e
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-08 14:41:45 +0200

    Add registration and update login form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,13 +3,29 @@ const AuthPage = () => {
     <>
       <div>
         <h1>Login or register</h1>
-        <form action="">
+        <div className="flex justify-around">
+          <form action="" className="flex flex-col" >
+            <header>Login</header>
             <input type="text" name="username" />
             <label htmlFor="username">Username</label>
-        </form>
+            <input type="text" name="password" />
+            <label htmlFor="password">Password</label>
+            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Submit</button>
+          </form>
+          <form action="" className="flex flex-col">
+            <header>Register</header>
+            <input type="text" name="username" />
+            <label htmlFor="username">Username</label>
+            <input type="text" name="password" />
+            <label htmlFor="password">Password</label>
+            <input type="text" name="confirmPassword" />
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Submit</button>
+          </form>
+        </div>
       </div>
     </>
   );
 };
 
-export default AuthPage
+export default AuthPage;
