706e2fd375a7898094f1a88c3eb45b2aa644bbd3
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-08 15:13:30 +0200

    Add User model & install JWT library

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -10,17 +10,17 @@ const AuthPage = () => {
             <label htmlFor="username">Username</label>
             <input type="text" name="password" />
             <label htmlFor="password">Password</label>
-            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Submit</button>
+            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Log in</button>
           </form>
           <form action="" className="flex flex-col">
-            <header>Register</header>
+            <header>Registration</header>
             <input type="text" name="username" />
             <label htmlFor="username">Username</label>
             <input type="text" name="password" />
             <label htmlFor="password">Password</label>
             <input type="text" name="confirmPassword" />
             <label htmlFor="confirmPassword">Confirm Password</label>
-            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Submit</button>
+            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Sign up</button>
           </form>
         </div>
       </div>
