8f056cba1b31d0d74fc60492c22300c70bbf2637
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-08 14:36:14 +0200

    Add auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,15 @@
+const AuthPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Login or register</h1>
+        <form action="">
+            <input type="text" name="username" />
+            <label htmlFor="username">Username</label>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AuthPage
