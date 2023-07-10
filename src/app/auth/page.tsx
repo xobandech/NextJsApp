cf5bea946e87cd5e9e5755598485189563e6a82f
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-07-10 15:11:46 +0200

    Add User model to db

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,27 +1,64 @@
+"use client";
+import { User } from "@/types/User";
+import { useEffect, useState } from "react";
+import { handleLogin, handleRegistration, getUsers } from "./serverFunctions";
+
 const AuthPage = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getUsers()
+      .then((users) => {
+        setUsers(users);
+        console.log(users);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
   return (
     <>
       <div>
+       
         <h1>Login or register</h1>
         <div className="flex justify-around">
-          <form action="" className="flex flex-col" >
+          <form action={handleLogin} className="flex flex-col">
             <header>Login</header>
-            <input type="text" name="username" />
             <label htmlFor="username">Username</label>
-            <input type="text" name="password" />
+            <input className="text-black" type="text" name="username" />
             <label htmlFor="password">Password</label>
-            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Log in</button>
+            <input className="text-black" type="text" name="password" />
+            <button
+              type="submit"
+              className="outline outline-2 outline-white bg-[#443]"
+            >
+              Log in
+            </button>
           </form>
-          <form action="" className="flex flex-col">
+          <form className="flex flex-col" action={handleRegistration}>
             <header>Registration</header>
-            <input type="text" name="username" />
             <label htmlFor="username">Username</label>
-            <input type="text" name="password" />
+            <input className="text-black" type="text" name="username" />
             <label htmlFor="password">Password</label>
-            <input type="text" name="confirmPassword" />
+            <input className="text-black" type="text" name="password" />
             <label htmlFor="confirmPassword">Confirm Password</label>
-            <button type="submit" className="outline outline-2 outline-white bg-[#443]">Sign up</button>
+            <input className="text-black" type="text" name="confirmPassword" />
+            <button
+              type="submit"
+              className="outline outline-2 outline-white bg-[#443]"
+            >
+              Sign up
+            </button>
           </form>
+        </div>
+        <div>
+        {users.map((user: User) => (
+          <div key={user.id}>
+            <p>{user.username}</p>
+            <p>{user.password}</p>
+          </div>
+        ))}
         </div>
       </div>
     </>
